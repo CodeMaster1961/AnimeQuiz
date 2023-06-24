c0e773cb3fa3b6deed73d5495d870d014ac530e3
omer <omer.aynaci@hva.nl>
2023-06-24 15:34:49 +0200

    image added

---
+++ app/src/main/java/com/example/animequizapp/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.animequizapp
+
+object Constants {
+    fun getQuestions(): ArrayList<Question> {
+        val questionsList = ArrayList<Question>()
+        val question1 = Question(
+            1, "What is the name of this Anime Character ?",
+
+
+        )
+    }
+
+}
--- app/src/main/java/com/example/animequizapp/MainActivity.kt
+++ app/src/main/java/com/example/animequizapp/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.animequizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton: Button = findViewById(R.id.buttonStart)
+        val inputText: EditText = findViewById(R.id.inputText)
+
+        startButton.setOnClickListener {
+            if (inputText.text.isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show()
+            } else {
+                val intent = Intent(this,QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
 
     }
 }

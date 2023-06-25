82a2842a43055c987808f9be0e54fc6fe692ea05
omer <omer.aynaci@hva.nl>
2023-06-25 15:05:42 +0200

    function created and an drawable added

--- app/src/main/java/com/example/animequizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/animequizapp/QuizQuestionsActivity.kt
@@ -3,13 +3,64 @@ package com.example.animequizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), OnClickListener {
+
+    private var mCurrentPosition: Int = 1
+    private var mQuestionsList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
+
+    private var progressBar: ProgressBar? = null
+    private var textViewProgress: TextView? = null
+    private var textViewQuestion: TextView? = null
+    private var imageViewImage: ImageView? = null
+
+    private var textViewOptionOne: TextView? = null
+    private var textViewOptionTwo: TextView? = null
+    private var textViewOptionThree: TextView? = null
+    private var textViewOptionFour: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
+        progressBar = findViewById(R.id.progressBar)
+        textViewProgress = findViewById(R.id.tvProgress)
+        textViewQuestion = findViewById(R.id.question)
+        imageViewImage = findViewById(R.id.ivImage)
+        textViewOptionOne = findViewById(R.id.tvOptionOne)
+        textViewOptionTwo = findViewById(R.id.tvOptionTwo)
+        textViewOptionThree = findViewById(R.id.tvOptionThree)
+        textViewOptionFour = findViewById(R.id.tvOptionFour)
+
+        setQuestion()
+    }
+
+    private fun setQuestion() {
         val questionsList = Constants.getQuestions()
         Log.i("QuestionsList Size is", "${questionsList.size}")
+
+        for (i in questionsList) {
+            Log.e("Questions", i.question)
+        }
+
+        var currentPosition = 1
+        val question: Question = questionsList[currentPosition - 1]
+        imageViewImage?.setImageResource(question.image)
+        progressBar?.progress = currentPosition
+        textViewProgress?.text = "$currentPosition/${progressBar?.max}"
+        textViewQuestion?.text = question.question
+        textViewOptionOne?.text = question.optionOne
+        textViewOptionTwo?.text = question.optionTwo
+        textViewOptionThree?.text = question.optionThree
+        textViewOptionFour?.text = question.optionFour
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }

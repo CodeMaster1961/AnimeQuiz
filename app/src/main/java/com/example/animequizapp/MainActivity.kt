29225cdc74d171d5c16673563b5d4a7f2d1811ea
omer <omer.aynaci@hva.nl>
2023-09-12 12:35:19 +0200

    comments added

--- app/src/main/java/com/example/animequizapp/MainActivity.kt
+++ app/src/main/java/com/example/animequizapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
